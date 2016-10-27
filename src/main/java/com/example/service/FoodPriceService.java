00179af41c93178d7cec8016d4439b99e9b43a1f
nishida4e <s_nishida@four-e.co.jp>
2016-10-27 20:37:03 +0900

    sql

--- src/main/java/com/example/service/FoodPriceService.java
+++ src/main/java/com/example/service/FoodPriceService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.domain.FoodPrice;
+import com.example.repository.FoodPriceJdbcRepository;
 import com.example.repository.FoodPriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,24 +10,25 @@ import java.util.List;
 
 @Service
 public class FoodPriceService {
-	
-    @Autowired
-    FoodPriceRepository fpRepo;
 
-    public List<FoodPrice> findAll() {
-        return fpRepo.findAll();
-    }
+	@Autowired
+	// FoodPriceRepository fpRepo;
+	FoodPriceJdbcRepository fpRepo;
 
-    public FoodPrice findOne(Integer id) {
-        return fpRepo.findOne(id);
-    }
+	public List<FoodPrice> findAll() {
+		return fpRepo.findAll();
+	}
 
-    public FoodPrice create(FoodPrice fp) {
-        return fpRepo.save(fp);
-    }
+	public FoodPrice findOne(Integer id) {
+		return fpRepo.findOne(id);
+	}
 
-    public void delete(Integer id) {
-    	fpRepo.delete(id);
-    }
+	public FoodPrice create(FoodPrice fp) {
+		return fpRepo.save(fp);
+	}
+
+	public void delete(Integer id) {
+		fpRepo.delete(id);
+	}
 
 }
